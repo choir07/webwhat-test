7cab861c1ed53547c58e0b231e56dff5f268b0be
choir07 <choir.2539@gmail.com>
2026-07-16 11:17:40 +0800

    fix: republish filament assets on every deploy

--- docker/start.sh
+++ docker/start.sh
@@ -1,21 +1,20 @@
 #!/bin/sh
 set -e
 
-echo '==> Publishing assets...'
+echo "==> Publishing Filament assets..."
 php artisan filament:assets
-php artisan vendor:publish --tag=livewire:assets --force
 
-echo '==> Caching config at runtime...'
+echo "==> Caching config at runtime..."
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
 
-echo '==> Running migrations...'
+echo "==> Running migrations..."
 php artisan migrate --force --graceful
 
-echo '==> Seeding Cloudinary URLs...'
+echo "==> Seeding Cloudinary URLs..."
 php artisan db:seed --class=CloudinaryUrlSeeder --force
 
-echo '==> Starting services...'
+echo "==> Starting services..."
 nginx
 exec php-fpm

83bd92c39c822d75ccbc9ea4b50548305c009b15
choir07 <choir.2539@gmail.com>
2026-07-16 11:48:30 +0800

    fix: force https scheme in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,8 +17,11 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
-        if (env('FORCE_HTTPS', false)) {
-            \URL::forceScheme('https');
-        }
+       
+    if (app()->environment('production')) {
+        \URL::forceScheme('https');
     }
+    
+    }
+
 }
